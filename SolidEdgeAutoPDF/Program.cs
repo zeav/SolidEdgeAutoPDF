3941cb94f407c9e82b2b8db846c8d79924f70489
Arne Velle <arne@zeav.no>
2021-12-14 09:22:25 +0100

    Add project files.

---
+++ SolidEdgeAutoPDF/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SolidEdgeAutoPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolidEdgeFramework.Application seApplication = null;
+            SolidEdgeFramework.Documents seDocuments = null;
+            SolidEdgeAssembly.AssemblyDocument seAssemblyDocument = null;
+
+            seApplication = solidedgeutils
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
